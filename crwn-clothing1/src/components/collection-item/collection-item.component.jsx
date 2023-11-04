15c6e7210e304d351fee312218c662f2df55c42c
Tajudeen <tajudeentaiwo13@gmail.com>
2023-11-04 22:42:07 +0100

    Adding Cart Adding Features, utils funct and others

--- crwn-clothing1/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing1/src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import './collection-item.styles.scss'
+import { connect } from 'react-redux';
 
-const CollectionItem = ({id, name, price, imageUrl}) =>(
-    <div className='collection-item'>
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({ item, addItem }) =>{
+    const { name, price, imageUrl } = item;
+    return(
+        <div className='collection-item'>
         <div className="image" style={{
             backgroundImage: `url(${imageUrl})`
         }}
@@ -11,7 +17,22 @@ const CollectionItem = ({id, name, price, imageUrl}) =>(
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+       
+        <CustomButton onClick={() => addItem(item)} inverted>
+            Add to Cart
+        </CustomButton>
     </div>
 );
+    
+        
+    };
 
-export default CollectionItem;
+const mapStateToProps = dispatch =>({
+    addItem: item => dispatch(addItem(item))
+});
+   
+
+export default  connect(
+    null,
+    mapStateToProps
+)(CollectionItem);
